ec8594e8c787f96bfe84b7b77e5c8f22d21f76d0
mariev718_lpsk12 <mariev718@lpsk12.org>
2024-04-30 08:45:29 -0600

    upload script started

---
+++ upload.js
@@ -0,0 +1,10 @@
+// document.getElementById('upload-form').addEventListener('file', e => {
+//     e.preventDefault()
+//     const file = document.getElementById('file').files[0]
+//     Storage.put(file.name, file)
+//     .then(item => {
+//         console.log(item)
+//     }0
+//     .catch(err => console.error(err))
+//     })
+// })
