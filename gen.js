8a74fd35b2bd9b9ea96ea9bbcbddddadee13218e
nxthyn <nathanh236@lpsk12.org>
2024-05-01 10:14:36 -0600

    blank?

--- gen.js
+++ gen.js
@@ -1,5 +1,7 @@
 function bonnify() {
 
+    document.body.style.display = "none";
+
     document.body.style.backgroundImage = "url('unnamed.jpg')";
     document.body.style.backgroundSize = "100% 100%";
 
