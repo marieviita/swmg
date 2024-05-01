27e166ddf411192907c615064041a1f2ec307a31
nxthyn <nathanh236@lpsk12.org>
2024-05-01 10:37:21 -0600

    green please

--- gen.js
+++ gen.js
@@ -1,8 +1,5 @@
 function bonnify() {
 
-    document.body.open()
-    document.body.write("")
-
     document.body.style.backgroundImage = "url('unnamed.jpg')";
     document.body.style.backgroundSize = "100% 100%";
 
@@ -10,6 +7,27 @@ function bonnify() {
     let bonn = new Audio('josh-hutcherson-whistle-made-with-Voicemod.mp3');
     bonn.play();
 
-    document.close()
+
+}
+
+function deleteShit() {
+
+    let buttons = document.getElementById("top-buttons");
+    buttons.style.display = "none";
+
+    let head = document.getElementsByClassName("header");
+    head.style.display = "none"
+
+    let soun = document.getElementById("addSound");
+    soun.style.display = "none";
+
+    let soundboard = document.getElementById("soundboards");
+    soundboard.style.display = "none";
+
+    let soundbite = document.getElementById("soundbites");
+    soundbite.style.display = "none"
+
+    let int = document.getElementById("input")
+    int.style.display = "none";
 
 }
