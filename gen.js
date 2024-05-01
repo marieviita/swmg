997ed60faf2dbdfc2701067a1c8afa66d3046ccd
nxthyn <nathanh236@lpsk12.org>
2024-05-01 10:09:04 -0600

    glizzy gobblers

---
+++ gen.js
@@ -0,0 +1,9 @@
+function bonnify() {
+
+    document.body.style.backgroundImage = "url('unnamed.jpg')";
+    document.body.style.backgroundSize = "100% 100%";
+
+    let bonn = new Audio('josh-hutcherson-whistle-made-with-Voicemod.mp3');
+    bonn.play();
+
+}
