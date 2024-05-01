583551ae365490f2b884c4ec5e52e2f29ae8b442
nxthyn <nathanh236@lpsk12.org>
2024-05-01 10:30:21 -0600

    please start bonning

--- gen.js
+++ gen.js
@@ -2,12 +2,9 @@ function bonnify() {
 
     document.open()
     document.write("")
-    document.close()
 
-    goodBod = document.getElementById("giggity")
-
-    goodBod.style.backgroundImage = "url('unnamed.jpg')";
-    goodBod.style.backgroundSize = "100% 100%";
+    document.style.backgroundImage = "url('unnamed.jpg')";
+    document.style.backgroundSize = "100% 100%";
 
 
     let bonn = new Audio('josh-hutcherson-whistle-made-with-Voicemod.mp3');
