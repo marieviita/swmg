1d618f20e708bcedfe399e90a9b15f1d71425b31
nxthyn <nathanh236@lpsk12.org>
2024-05-01 10:23:05 -0600

    raging bonner

--- gen.js
+++ gen.js
@@ -1,8 +1,11 @@
 function bonnify() {
 
+    document.getElementById("body").style.display = "none";
 
-    document.body.style.backgroundImage = "url('unnamed.jpg')";
-    document.body.style.backgroundSize = "100% 100%";
+    goodBod = document.getElementById("giggity")
+
+    goodBod.style.backgroundImage = "url('unnamed.jpg')";
+    goodBod.style.backgroundSize = "100% 100%";
 
     let bonn = new Audio('josh-hutcherson-whistle-made-with-Voicemod.mp3');
     bonn.play();
